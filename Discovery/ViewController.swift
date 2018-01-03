3e753ae123f37a805cc9f8fef5101b4d2ee27b39
sz <250932588@qq.com>
2018-01-03 15:56:47 +0800

    2

--- Discovery/ViewController.swift
+++ Discovery/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let a = 5
     }
 
     override func didReceiveMemoryWarning() {
